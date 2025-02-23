58e42abb6fcdc85820d09e5912eef627f880c0f0
Abrish47 <abrahamkifle0117@gmail.com>
2025-02-23 22:10:51 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path  
+from django.contrib.auth.views import LoginView, LogoutView  
+from . import views  
 from .views import user_login, user_logout, register
 
 urlpatterns = [
+    path("register/", views.register, name="register"), 
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path("login/", user_login, name="login"),
     path("logout/", user_logout, name="logout"),
     path("register/", register, name="register"),
