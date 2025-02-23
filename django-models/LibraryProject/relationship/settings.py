f511743ecb13167a2909d01fc406470c9c63b88a
Abrish47 <abrahamkifle0117@gmail.com>
2025-02-23 22:16:33 +0300

    Update settings.py

--- django-models/LibraryProject/relationship/settings.py
+++ django-models/LibraryProject/relationship/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'relationship.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "relationship_app/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
