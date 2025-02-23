5f9cc30ba03f806051e415d0dd4f9993cac9a054
Abrish47 <abrahamkifle0117@gmail.com>
2025-02-23 21:55:04 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.views.generic import DetailView
-from django.views.generic.detail import DetailView
-from .models import Book, Library 
-from .models import Library  # Import both Book and Library models
-
-from django.shortcuts import render
-from .models import Book
 
 # Function-based view for user login
 def user_login(request):
@@ -37,19 +30,3 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
-
-def list_books(request):
-    books = Book.objects.all()  # Fetch all books
-    return render(request, "relationship_app/list_books.html", {"books": books})
-
-
-# Class-based view to display details of a specific library
-class LibraryDetailView(DetailView):
-    model = Library  # Use the Library model
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # Add additional context if needed
-        return context
