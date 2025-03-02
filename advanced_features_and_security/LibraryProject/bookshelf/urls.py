068f8650549e41698761c6ca5872711f6bc3fd63
Abrish47 <abrahamkifle0117@gmail.com>
2025-03-02 20:20:49 +0300

    Update urls.py

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from . import views
 
 app_name = 'bookshelf'
@@ -8,4 +9,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='bookshelf/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='bookshelf/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
+    path('books/', views.list_books, name='list_books'),
+    path('books/create/', views.create_book, name='create_book'),
+    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
