0535cdb8072c3fef273195898960754f3f5eccf5
Abrish47 <abrahamkifle0117@gmail.com>
2025-03-02 20:16:09 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import permission_required
 from .models import CustomUser
+from .models import Book
 
 def register(request):
     if request.method == 'POST':
@@ -14,3 +17,34 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'bookshelf/register.html', {'form': form})
+
+# Permission-protected views
+@permission_required('bookshelf.can_view', raise_exception=True)
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/list_books.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        Book.objects.create(title=title)
+        return redirect('bookshelf:list_books')
+    return render(request, 'bookshelf/create_book.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.save()
+        return redirect('bookshelf:list_books')
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('bookshelf:list_books')
+    return render(request, 'bookshelf/delete_book.html', {'book': book})
