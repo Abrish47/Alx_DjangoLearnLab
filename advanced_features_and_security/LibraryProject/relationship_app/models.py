00fea6fbcd6a4e3c6a96c9a93cb347eabbf4ee74
Abrish47 <abrahamkifle0117@gmail.com>
2025-03-02 19:35:28 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,10 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
-# Author Model
+# Custom User Manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email=None, password=None, date_of_birth=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, date_of_birth=date_of_birth, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, date_of_birth=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, date_of_birth, **extra_fields)
+
+# Custom User Model
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+        
 class Author(models.Model):
     name = models.CharField(max_length=100)
-
     def __str__(self):
         return self.name
 
@@ -12,7 +44,6 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
     def __str__(self):
         return self.title
 
@@ -27,14 +58,15 @@ class Book(models.Model):
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
-
     def __str__(self):
         return self.name
 # Librarian Model
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
+    def __str__(self):
+        return self.name
+        
 class Author(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
@@ -65,11 +97,7 @@ class UserProfile(models.Model):
         ('Librarian', 'Librarian'),
         ('Member', 'Member'),
     )
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)  # Updated to CustomUser
     role = models.CharField(max_length=10, choices=ROLES, default='Member')
-
     def __str__(self):
         return f"{self.user.username} - {self.role}"
-
-    def __str__(self):
-        return self.name
