202202435f227b009db88214e5135684832022f8
Abrish47 <abrahamkifle0117@gmail.com>
2025-03-02 20:06:19 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .models import CustomUser
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('bookshelf:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'bookshelf/register.html', {'form': form})
