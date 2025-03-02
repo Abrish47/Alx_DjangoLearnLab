a3ea01382569a96c8027cbe62744d0828a1fce77
Abrish47 <abrahamkifle0117@gmail.com>
2025-03-02 20:27:48 +0300

    Update urls.py

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='bookshelf/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='bookshelf/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
-    path('books/', views.list_books, name='list_books'),
+    path('books/', views.book_list, name='book_list'),
     path('books/create/', views.create_book, name='create_book'),
     path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
     path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
