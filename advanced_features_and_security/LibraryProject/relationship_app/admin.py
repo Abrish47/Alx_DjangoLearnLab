c7dfc61ede07d601ca47d2d2f14fc0db6aea10db
Abrish47 <abrahamkifle0117@gmail.com>
2025-03-02 19:37:35 +0300

    Update admin.py

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Author, Book, Library, Librarian, UserProfile
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Information', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Information', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+# Register models
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
+admin.site.register(UserProfile)
