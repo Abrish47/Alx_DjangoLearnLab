0ae7c824821b3176730ae51231056b422afce2d7
Abrish47 <abrahamkifle0117@gmail.com>
2025-03-15 22:15:49 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,11 +2,14 @@ from rest_framework import generics.ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets.ModelViewSet
+from rest_framework import permissions.IsAuthenticated
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Add this
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated] 
