e9f6c113c7e7e452040ad28ae35277d5042ad3e5
Abraham Kifle <abrahamkifle0117@gmail.com>
2025-03-20 23:39:25 +0300

    update forms and views

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -8,3 +8,8 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["email"]
