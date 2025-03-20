2e4404f0c540d2e9e270ffcf749587462a59f661
Abraham Kifle <abrahamkifle0117@gmail.com>
2025-03-20 23:23:28 +0300

    created urls for the blog app

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Auto-login after registration
+            return redirect("home")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "blog/register.html", {"form": form})
+
+def home(request):
+    return render(request, "blog/home.html")
